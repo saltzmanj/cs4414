338a61690b1ea64c1c036a344f93e3b6e39120dd
Jake Saltzman <jss7kj@virginia.edu>
2017-02-14 14:35:12 -0500

    added regex for commands

--- shell/src/jsh.c
+++ shell/src/jsh.c
@@ -3,7 +3,14 @@
 int debuglinect = 0;
 
 error_t InitJsh() {
+	int status;
+
 	stdinbuf_global = (char*) malloc(sizeof(char) * (MAX_CHARS_PER_LINE+1));
+	status = regcomp(&pregex, ALLOWED_CHARS_REGEX, REG_EXTENDED|REG_NOSUB);
+
+	if (status != 0) {
+		printf("%s","pattern failed to compile");
+	}
 }
 
 error_t RunJsh() {
@@ -11,7 +18,10 @@ error_t RunJsh() {
 	printf("%d: ", debuglinect);
 	debuglinect += 1;
 	printf(PROMPT_STRING);
+	
 	runjsh_status = GrabLine(stdinbuf_global);
+	HandleError(runjsh_status);
+
 	runjsh_status = ParseLine(stdinbuf_global);
 	return runjsh_status;
 }
@@ -30,6 +40,7 @@ error_t GrabLine(char* targetbuf) {
 
 	while(1) {
 		current_char = fgetc(stdin);
+		// putchar(current_char);
 		if (current_char != '\n') {
 			*char_ptr = current_char;
 			char_ptr += sizeof(char);
@@ -42,6 +53,10 @@ error_t GrabLine(char* targetbuf) {
 		if (strsize > MAX_CHARS_PER_LINE) {
 			extra_chars_flag = 1;
 			grabline_status = TOO_MANY_CHARS_ERROR;
+			// printf("%d", strsize);
+			int c;
+			while ((c = getchar()) != EOF && c != '\n')
+            	continue;
 			break;
 		}
 	}
@@ -51,9 +66,25 @@ error_t GrabLine(char* targetbuf) {
 
 error_t ParseLine(char* srcbuf) {
 	error_t parse_status = INIT_STATUS;
+	int regmatch = 0;
+	int status = 0;
+
+	regmatch = regexec(&pregex, srcbuf, (size_t)0, NULL, 0);
+
+	if(regmatch != 0) {
+		parse_status = SYNTAX_ERR_ERROR;
+	}
+
 	if (strcmp(srcbuf, EXIT_CMD) == 0) {
 		parse_status = EXIT_ERROR;
+		return parse_status;
 	}
 
 	return parse_status;
+}
+
+void HandleError(error_t errorin) {
+	if (errorin.eflag == 1) {
+		printf("ERROR: %s\n", errorin.msg);
+	}
 }
--- shell/src/jsh.h
+++ shell/src/jsh.h
@@ -1,14 +1,18 @@
-#ifndef __JSH_
-#define __JSH_
+#ifndef __JSH_H
+#define __JSH_H
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <regex.h>
 
 // Parameters 
 #define MAX_CHARS_PER_LINE 64
 #define STAT_MESSAGE_MAX_CHARS 64
 #define PROMPT_STRING "jsh$ "
+#define ALLOWED_CHARS_REGEX (const char*) "^[A-Za-z0-9[:space:]\\.\\-\\_]*([<|>[:space:]]*[A-Za-z0-9\\._]+[A-Za-z0-9\\.\\-_[:space:]]*)*$"
+
+// Shell specific commands
 #define EXIT_CMD "exit"
 
 // Constants related to error handling
@@ -16,6 +20,9 @@
 
 #define TOO_MANY_CHARS_MSG "Line character limit exceeded."
 #define TOO_MANY_CHARS_ERROR (error_t) {1, 101, TOO_MANY_CHARS_MSG, 0}
+
+#define SYNTAX_ERR_MSG "Syntax Error."
+#define SYNTAX_ERR_ERROR (error_t) {1, 102, SYNTAX_ERR_MSG, 0}
 
 #define EXIT_MSG "Goodbye!"
 #define EXIT_ERROR (error_t) {0, 0, EXIT_MSG, 1}
@@ -30,11 +37,12 @@ typedef struct {
 } error_t;
 
 char* stdinbuf_global;
+regex_t pregex;
 
 error_t InitJsh();
 error_t RunJsh();
 error_t GrabLine(char* targetbuf);
 void ExitJsh();
 error_t ParseLine(char* srcbuf);
-
+void HandleError(error_t errorin);
 #endif
