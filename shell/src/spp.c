8e14626cb257699ed93780d794cfdc21872c8918
Jake Saltzman <jss7kj@virginia.edu>
2017-02-19 23:15:29 -0500

    major refactor - now spp!

--- shell/src/spp.c
+++ shell/src/spp.c
@@ -2,7 +2,7 @@
 // ----------------------------------- Main
 
 int main() {
-
+	printf(STARTUP_STRING);
 	SppRun();
 
 	return 0;
@@ -26,15 +26,76 @@ void SppRun() {
 sppstate_t SppIterate(sppstate_t statein, sdata_t* sdata, error_t* serror) {
 	sppstate_t nextstate = SHUTDOWN;
 
+	// If an error is raised, rudely interrupt and move to error handler state
+	if(serror->eflag) {
+		nextstate = HANDLE_ERROR;
+	}
+
 	switch(statein){
 		case(INITIALIZE): {
+			debug_print("Initializing...\n");
+			int status;
+			// Initialize the stdin buffer			
+			sdata->stdin_buffer = (char*) malloc(sizeof(char)*MAX_CHARS_PER_LINE + 1);
+			sdata->stdin_buffer[MAX_CHARS_PER_LINE] = '\0';
+			status = regcomp(&pregex, ALLOWED_CHARS_REGEX, REG_EXTENDED|REG_NOSUB);
+			
+			if(status != 0) {
+				*serror = REGEX_ERROR;
+			}
+			nextstate = GETLINE;
+		} break;
+
+		case(HANDLE_ERROR): {
+			// debug_print("\tHandling an error...\n");
+			HandleError(serror);
+			if(serror->fatal) {
+				nextstate = SHUTDOWN;
+			} else {
+				nextstate = GETLINE;
+			}
+
+			*serror = NO_ERROR;
 
 		} break;
-		
+		case(GETLINE): {
+			// debug_print("Getting a line... ");
+			printf("%s", PROMPT_STRING);
+			GrabLine(sdata->stdin_buffer, serror);
+			debug_print("\tDEBUGGING INFO\n\t------------\n");
+			debug_print("\tLine Entered: %s\n", sdata->stdin_buffer);
+			
+			nextstate = CHECK_SYNTAX;
+		} break;
+		case(CHECK_SYNTAX): {
+			syntax_t syntaxstatus = ERR;
+			CheckSyntax(sdata->stdin_buffer, &syntaxstatus, serror);
+
+			if(syntaxstatus == OK){
+				nextstate = EXTRACT_CMDS;
+				// debug_print("\tSyntax OK");
+			} else if (syntaxstatus == BLANK) {
+				nextstate = GETLINE;
+				// debug_print("\tBlank Line Entered");
+			} else {
+				nextstate = HANDLE_ERROR;
+				debug_print("\tSyntax error");
+			}
+		} break;
+		case(EXTRACT_CMDS): {
+			debug_print("\nExtracting commands...\n");
+			nextstate = EXECUTE;
+		} break;
+		case(EXECUTE):{
+			debug_print("\nExecuting Commands...\n");
+			nextstate = GETLINE;
+		} break;
 		case(SHUTDOWN): { 
-		
+			debug_print("\nShutting Down...\n");
+			free(sdata->stdin_buffer);
 		} break;
 	}
+
 	return nextstate;
 }
 
@@ -69,5 +130,93 @@ void FreeBuffer(char** args[], error_t* errorin) {
 			free((*args)[i]);
 			i += 1;
 		}
+	}
+}
+
+void GrabLine(char* targetbuf, error_t* serror) {
+	if(serror->eflag) {
+		return;
+	}
+
+	char current_char; // Buffer space for last char from command line
+	char* char_ptr = targetbuf; //Create a pointer so we can insert into the buffer
+	int strsize = 0; 
+	int extra_chars_flag = 0;
+	int sflag = 0;
+
+	while(1) {
+		current_char = fgetc(stdin);
+		// putchar(current_char);
+		if (current_char != '\n') {
+			*char_ptr = current_char;
+			char_ptr += sizeof(char);
+			strsize += 1;
+		} else {
+			*char_ptr = '\0';
+			break;
+		}
+
+		if (strsize > MAX_CHARS_PER_LINE) {
+			extra_chars_flag = 1;
+			*serror = TOO_MANY_CHARS_ERROR;
+			// printf("%d", strsize);
+			int c;
+			while ((c = getchar()) != EOF && c != '\n')
+            	continue;
+			break;
+		}
+	}
+
+}
+
+void CheckSyntax(char* srcbuf, syntax_t* syntaxstatus, error_t* serror) {
+	if(serror->eflag){
+		return;
+	}
+
+	char* strptr = srcbuf;
+	int regmatch = 0;
+	int status = 0;
+	int redirchar = -1;
+	regmatch = regexec(&pregex, srcbuf, (size_t) 0, NULL, 0);
+
+	if(srcbuf[0] == '\0'){
+		*syntaxstatus = BLANK;
+		return;
+	} 
+
+	if(regmatch != 0) {
+		*serror = SYNTAX_ERR_ERROR;
+		*syntaxstatus = ERR;
+		return;
+	}
+
+
+	while(*strptr != '\0') {
+		if (*strptr == '>') {
+			redirchar = '>';
+			strptr += sizeof(char);
+			continue;
+		} else if(*strptr == '<') {
+			redirchar == '<';
+			strptr += sizeof(char);
+			continue;
+		} else {
+			strptr += sizeof(char);
+		}
+
+		if (*strptr==redirchar) {
+			*serror = SYNTAX_ERR_DBLREDIR_ERROR;
+			*syntaxstatus = ERR;
+			return;
+		}
+
+	syntaxstatus = OK;
+
+}
+
+void HandleError(error_t *errorin) {
+	if (errorin->eflag == 1) {
+		printf("ERROR: %s\n", errorin->msg);
 	}
 }
--- shell/src/spp.h
+++ shell/src/spp.h
@@ -11,12 +11,12 @@
 // ---------------------------------------------------- Parameters 
 #define MAX_CHARS_PER_LINE 80
 #define STAT_MESSAGE_MAX_CHARS 64
-#define PROMPT_STRING "jsh$ "
+#define PROMPT_STRING "spp$ "
 #define ALLOWED_CHARS_REGEX (const char*) "^[A-Za-z0-9[:space:]\\.\\_\\-]+([<|>]?[[:space:]]* [A-Za-z0-9\\._]+[A-Za-z0-9[:space:]\\.\\_\\-]*)*$"
 #define WORD_DELIM " "
-
+#define STARTUP_STRING "shell++\nJake Saltzman\nCS4414 Spring 2017\n\n"
 // ---------------------------------------------------- Debugging stuff
-#define DEBUG 1
+#define DEBUG 0
 #define debug_print(...) \
 	do { if (DEBUG) fprintf(stdout, __VA_ARGS__); } while(0)
 
@@ -25,38 +25,55 @@
 
 // ------------------------------------------------------ Constants related to error handling
 
-#define NO_ERROR (error_t) {0, 0, "OK", 0}
+#define NO_ERROR (error_t) {0, 0, "OK", 0, 0}
 
 #define TOO_MANY_CHARS_MSG "Line character limit exceeded."
-#define TOO_MANY_CHARS_ERROR (error_t) {1, 101, TOO_MANY_CHARS_MSG, 0}
+#define TOO_MANY_CHARS_ERROR (error_t) {1, 101, TOO_MANY_CHARS_MSG, 0, 0}
 
 #define SYNTAX_ERR_MSG "Syntax Error."
-#define SYNTAX_ERR_ERROR (error_t) {1, 102, SYNTAX_ERR_MSG, 0}
+#define SYNTAX_ERR_ERROR (error_t) {1, 102, SYNTAX_ERR_MSG, 0, 0}
 
 #define SYNTAX_ERR_DBLREDIR_MSG "Syntax Error - Double Redirect."
-#define SYNTAX_ERR_DBLREDIR_ERROR (error_t) {1, 112, SYNTAX_ERR_DBLREDIR_MSG, 0}
+#define SYNTAX_ERR_DBLREDIR_ERROR (error_t) {1, 112, SYNTAX_ERR_DBLREDIR_MSG, 0, 0}
 
-#define EXIT_MSG "Goodbye!"
-#define EXIT_ERROR (error_t) {0, 0, EXIT_MSG, 1}
+// #define EXIT_MSG "Goodbye!"
+// #define EXIT_ERROR (error_t) {0, 0, EXIT_MSG, 1, 0}
 
 #define FORK_FAILED_MSG "Fork failed."
-#define FORK_FAILED_ERROR (error_t) {1, 201, FORK_FAILED_MSG, 0}
+#define FORK_FAILED_ERROR (error_t) {1, 201, FORK_FAILED_MSG, 0, 0}
 
 #define EXEC_FAILED_MSG "Target function failed to execute."
-#define EXEC_FAILED_ERROR (error_t) {1, 202, EXEC_FAILED_MSG, 0}
+#define EXEC_FAILED_ERROR (error_t) {1, 202, EXEC_FAILED_MSG, 0, 0}
 
 #define UNKNOWN_MSG "An unknown error occurred."
-#define UNKNOWN_ERROR (error_t) {1, 999, UNKNOWN_MSG, 0}
+#define UNKNOWN_ERROR (error_t) {1, 999, UNKNOWN_MSG, 0, 0}
+
+#define REGEX_MSG "Regex failed to compile."
+#define REGEX_ERROR (error_t) {1, 301, REGEX_MSG, 0, 1}
+
+// -------------------------------------------------- Global Variables
+regex_t pregex;
 
 // -------------------------------------------------- Tyepdefs
 
 typedef enum {
 	INITIALIZE,
-	SHUTDOWN
+	GETLINE,
+	CHECK_SYNTAX,
+	EXTRACT_CMDS,
+	EXECUTE,
+	HANDLE_ERROR,
+	SHUTDOWN,
 } sppstate_t;
 
-typedef struct {
+typedef enum {
+	OK,
+	BLANK,
+	ERR
+} syntax_t;
 
+typedef struct {
+	char* stdin_buffer;
 } sdata_t;
 
 typedef struct {
@@ -64,7 +81,10 @@ typedef struct {
 	int ecode; // Error code if applicable
 	char msg[STAT_MESSAGE_MAX_CHARS]; // Error message
 	int xflag; // 1 if exit, 0 elsewise
+	int fatal; // 1 if the error is fatal, 0 otherwise
 } error_t;
+
+
 
 #define SDATA_INIT (sdata_t) {}
 
@@ -72,9 +92,13 @@ typedef struct {
 void SppRun();
 sppstate_t SppIterate(sppstate_t statein, sdata_t* sdata, error_t* serror);
 
+// -------------------------------------------------- Key Functions
+void GrabLine(char* targetbuf, error_t* serror);
+void CheckSyntax(char* srcbuf, syntax_t* syntaxstatus, error_t* serror);
 // -------------------------------------------------- Utility Functions
 
 int CountWords(char* string);
 void FreeBuffer(char** args[], error_t* errorin);
+void HandleError(error_t *errorin);
 
 #endif
